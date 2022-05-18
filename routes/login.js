a592ccfa12745bb015c3a7393e5e18c0f3b834be
JSbad <patryk@ctemplar.com>
2022-05-18 22:28:11 +0100

    bugfixing

--- routes/login.js
+++ routes/login.js
@@ -5,23 +5,17 @@ const md5 = require('md5');
 
 
 login.post('/', async (req, res) => {
-    var reqUser = req.body.username;
-    var reqPass = req.body.password;
+    let reqUser = req.body.username;
+    let reqPass = req.body.password;
     if (req.body != null && reqUser != null && reqPass != null) {
         const [results, error] = await User.getBy("username", reqUser);
         let hashedPassword = md5(reqPass);
         if ((results.length != 0 && error.length == 0) && (results[0].password == hashedPassword)) {
-            res.status(400).json(response.prepare(400, "Logged in", ""))
+            res.status(200).json(response.prepare(200, results[0].user_id, ""))
         } else { res.status(401).json(response.prepare(401, "", "Wrong username or password")) };
     } else { res.status(401).json(response.prepare(401, "", "Wrong username or password")) };
 });
 
-login.get('/logout', async (req, res) => {
-        if (err) {
-            console.log(err);
-        }
-        res.redirect('/');
-    
-});
+
 
 module.exports = login;
